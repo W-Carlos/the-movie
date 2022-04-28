218f8dd5e6c3e69fdc794f8ba986598a60f0f388
Wend Carlos <wendcarlos18@hotmail.com>
2022-04-28 19:32:20 -0300

    Remove some items, installed yarn, styled-components, created global styles and main page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const App = () => {
+
+  return (
+
+      <Container>
+        <h1>Hello Wolrd</h1>
+      </Container>
+    
+  )
+
+}
+
+export default App
