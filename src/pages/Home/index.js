025931d074d0e74835d0ce86b880108ce6f3f6c1
Wend Carlos <wendcarlos18@hotmail.com>
2022-04-30 16:42:28 -0300

    Rendering data dynamically

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,30 +4,42 @@ import { Container, ListOfMovie, ListItem } from "./styles";
 
 const App = () => {
 
+  const movies = [
+    {
+      id: 1,
+      title: 'Injustice',
+      image_url: "https://img.elo7.com.br/product/zoom/2F5195B/poster-impresso-avengers-design.jpg"
+    },
+    {
+      id: 2,
+      title: 'Batman',
+      image_url: "https://img.elo7.com.br/product/zoom/2F5195B/poster-impresso-avengers-design.jpg"
+    },
+    {
+      id: 3,
+      title: 'Spider-Man',
+      image_url: "https://img.elo7.com.br/product/zoom/2F5195B/poster-impresso-avengers-design.jpg"
+    },
+  ]
+
   return (
 
       <Container>
         <h1>The Movie</h1>
 
         <ListOfMovie>
-          <ListItem>
-            <a href="https://google.com">
-              <img src="https://img.elo7.com.br/product/zoom/2F5195B/poster-impresso-avengers-design.jpg" alt="Poster Avengers" />
-            </a>
-            <span>Avengers</span>
-          </ListItem>
-          <ListItem>
-            <a href="https://google.com">
-              <img src="https://img.elo7.com.br/product/zoom/2F5195B/poster-impresso-avengers-design.jpg" alt="Poster Avengers" />
-            </a>  
-            <span>Avengers</span>
-          </ListItem>
-          <ListItem>
-            <a href="https://google.com">
-              <img src="https://img.elo7.com.br/product/zoom/2F5195B/poster-impresso-avengers-design.jpg" alt="Poster Avengers" />
-            </a>
-            <span>Avengers</span>
-          </ListItem>
+          
+          {movies.map((movie) => (
+
+              <ListItem key={movie.id}>
+                <a href="https://google.com">
+                  <img src={movie.image_url} alt={movie.title} />
+                </a>
+                <span>{movie.title}</span>
+              </ListItem>
+
+          ))}
+
         </ListOfMovie>
       </Container>
     
